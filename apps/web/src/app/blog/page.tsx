6c4fb12cc0489f0088c20a4fdc976c2e2223d892
Casey Marksberry <caseymarksberry@gmail.com>
2025-03-19 15:39:55 -0400

    fix: update blog page to use correct Blog type

--- apps/web/src/app/blog/page.tsx
+++ apps/web/src/app/blog/page.tsx
@@ -7,6 +7,10 @@ import { queryBlogIndexPageData } from "@/lib/sanity/query";
 import type { QueryBlogIndexPageDataResult } from "@/lib/sanity/sanity.types";
 import { getMetaData } from "@/lib/seo";
 
+type Blog = NonNullable<
+  NonNullable<QueryBlogIndexPageDataResult>["blogs"]
+>[number];
+
 /**
  * Fetches blog posts data from Sanity CMS
  */
@@ -64,13 +68,9 @@ export default async function BlogIndexPage() {
         {/* Blog Grid */}
         {remainingBlogs.length > 0 && (
           <div className="grid grid-cols-1 gap-8 md:gap-12 lg:grid-cols-2">
-            {remainingBlogs.map(
-              (
-                blog: NonNullable<QueryBlogIndexPageDataResult>["blogs"][number],
-              ) => (
-                <BlogCard key={blog._id} blog={blog} />
-              ),
-            )}
+            {remainingBlogs.map((blog: Blog) => (
+              <BlogCard key={blog._id} blog={blog} />
+            ))}
           </div>
         )}
       </div>
